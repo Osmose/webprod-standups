1998cc2657beb6b0c6449784cbc0be48ff64d105
Michael Kelly <mkelly@mozilla.com>
2012-07-02 11:47:11 -0400

    Add ability to submit standup from app.

--- js/standup.js
+++ js/standup.js
@@ -4,6 +4,7 @@
 
     // zomg global variables
     var references = {};
+    var user_id = null;
 
     var converter = Markdown.getSanitizingConverter();
 
@@ -13,11 +14,13 @@
     yam.getLoginStatus(function(response) {
         if (response.authResponse) {
             // User is logged in, init app
+            user_id = response.access_token.user_id;
             init();
         } else {
             // User is not logged in, let's log them in!
             yam.login(function(response) {
                 if (response.authResponse) {
+                    user_id = response.access_token.user_id;
                     init();
                 } else {
                     alert('Sorry, you need to log into Yammer to use this!');
@@ -39,8 +42,14 @@
                     references[reference.id] = reference;
                 });
 
+                var now = new Date();
+                var today = now.setHours(0, 0, 0, 0);
                 processMessages(response.messages.filter(function(msg) {
-                    return msg.group_id === WEBPROD_GROUP;
+                    // Use setHours(0, 0, 0, 0) to compare the day portion of
+                    // each date so that only standups for the current day are
+                    // shown.
+                    var msg_day = new Date(msg.created_at).setHours(0, 0, 0, 0);
+                    return msg.group_id === WEBPROD_GROUP && msg_day == today;
                 }));
             }
         });
@@ -49,6 +58,7 @@
     function processMessages(messages) {
         var $messages = $('#messages');
         var senders = {};
+        var my_standup = null;
 
         for (var k = 0; k < messages.length; k++) {
             var msg = messages[k];
@@ -56,6 +66,17 @@
 
             // Check if we've already gotten a standup from this person.
             if (senders.hasOwnProperty(sender.id)) {
+                continue;
+            } else if (sender.id === user_id) {
+                // Only use most recent standup.
+                if (my_standup === null) {
+                    my_standup = msg;
+                    $('#my-standup').empty().append(ich.message({
+                        name: 'My Standup',
+                        mugshot_href: sender.mugshot_url,
+                        body: converter.makeHtml(msg.body.plain)
+                    }));
+                }
                 continue;
             } else {
                 senders[sender.id] = true;
@@ -70,12 +91,52 @@
 
         // Remove progress bar.
         $('#progress').fadeOut(500, function() {
-            $messages
-                .fadeIn(500)
-                .masonry({
-                    itemSelector : '.message',
-                    columnWidth : 470
-                });
+            $('#app-container').fadeIn(500);
+            $messages.masonry({
+                itemSelector : '.message',
+                columnWidth : 470
+            });
         });
     }
+
+    $(function() {
+        var $my_standup = $('#my-standup');
+
+        $('#standup-body').on('change keydown keyup', function(e) {
+            $('#preview').empty().append(ich.message({
+                name: 'My Standup',
+                body: converter.makeHtml($(this).val())
+            }));
+        }).change();
+
+        $('#standup-form').on('submit', function(e) {
+            var standup = $('#standup-body').val();
+
+            $('#post-button').attr('disabled', 'disabled');
+
+            yam.request({
+                url: '/api/v1/messages.json',
+                method: 'POST',
+                data: {
+                    body: standup,
+                    topic1: 'Standup',
+                    group_id: WEBPROD_GROUP
+                },
+                success: function(response) {
+                    var msg = response.messages[0];
+                    var sender = references[msg.sender_id];
+
+                    $my_standup.fadeOut(500, function() {
+                        $my_standup.empty().append(ich.message({
+                            name: 'My Standup',
+                            mugshot_href: sender.mugshot_url,
+                            body: converter.makeHtml(msg.body.plain)
+                        })).fadeIn(500);
+                    });
+                }
+            });
+
+            e.preventDefault();
+        });
+    });
 })(jQuery, yam, config);
